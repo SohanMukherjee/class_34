3821e0579f2536afa8171e787d179ba800bc9afb
SohanMukherjee <73114040+SohanMukherjee@users.noreply.github.com>
2020-12-10 18:30:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var ball;
+var db,position;
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    db = firebase.database();
+    db.ref("Ball/Position").on("value",readPosition,showError)
+}
+
+function draw(){
+    background("white");
+    if(position){
+        if(keyDown(LEFT_ARROW)){
+            writePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            writePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+1);
+        }
+        drawSprites();
+    }
+    
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+function showError(){
+    console.log("error")
+}
+
+function readPosition(data){
+    position = data.val()
+    ball.x = position.X
+    ball.y = position.Y
+}
+
+function writePosition(x,y){
+    db.ref("Ball/Position").set({
+        X:position.X + x,
+        Y:position.Y + y
+    })
+}
